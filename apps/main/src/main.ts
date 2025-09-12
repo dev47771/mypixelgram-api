85af72fe4d0f018ad1ff5aa3823ca8c93d58fd6a
Ilya Anisimkov <oblak.00@bk.ru>
2025-09-13 00:23:06 +0500

    delete log in main.ts

--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('main is started');
   console.log('main application started on port 3000');
   await app.listen(process.env.PORT ?? 3000);
 }
