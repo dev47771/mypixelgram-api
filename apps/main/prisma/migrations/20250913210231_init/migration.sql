a0fb39f0202f7c6115cc21cac23dcdcac1191ba5
dariantonova <daricio1256@gmail.com>
2025-09-14 00:12:52 +0300

    feat(main): add env file

--- apps/main/prisma/migrations/20250913210231_init/migration.sql
+++ apps/main/prisma/migrations/20250913210231_init/migration.sql
@@ -30,8 +30,22 @@ CREATE TABLE "public"."password_recoveries" (
     CONSTRAINT "password_recoveries_pkey" PRIMARY KEY ("userId")
 );
 
+-- CreateTable
+CREATE TABLE "public"."user_providers" (
+    "id" SERIAL NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerUserId" TEXT NOT NULL,
+    "profile" JSONB,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "user_providers_pkey" PRIMARY KEY ("id")
+);
+
 -- AddForeignKey
 ALTER TABLE "public"."user_confirmations" ADD CONSTRAINT "user_confirmations_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "public"."password_recoveries" ADD CONSTRAINT "password_recoveries_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."user_providers" ADD CONSTRAINT "user_providers_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
