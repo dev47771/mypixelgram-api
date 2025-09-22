26bc74c9d65eb4623ae40cb25f955627f2e1f609
Ilya Anisimkov <oblak.00@bk.ru>
2025-09-23 00:00:38 +0500

    fix user repo

--- apps/main/src/modules/user-accounts/infrastructure/users.repo.ts
+++ apps/main/src/modules/user-accounts/infrastructure/users.repo.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../core/prisma/prisma.service';
-import { User as UserModel } from '@prisma/client';
+import {
+  User as UserModel,
+  PasswordRecovery as PasswordRecoveryModel,
+} from '@prisma/client';
 import { CreateUserRepoDto } from './dto/create-user.repo-dto';
 import { CreateUserConfirmationRepoDto } from './dto/create-user-confirmation.repo-dto';
 
