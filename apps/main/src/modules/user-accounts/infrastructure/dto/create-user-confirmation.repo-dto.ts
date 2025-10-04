3e26a2c2a93c8a261fff62a23832e7b7744d1c68
Alik <al.humbatli@gmail.com>
2025-10-04 11:00:12 +0400

    make isAgreeWithPrivacy? optional

--- apps/main/src/modules/user-accounts/infrastructure/dto/create-user-confirmation.repo-dto.ts
+++ apps/main/src/modules/user-accounts/infrastructure/dto/create-user-confirmation.repo-dto.ts
@@ -2,5 +2,5 @@ export class CreateUserConfirmationRepoDto {
   confirmationCode: string | null;
   expirationDate: Date | null;
   isConfirmed: boolean;
-  isAgreeWithPrivacy: boolean;
+  isAgreeWithPrivacy?: boolean;
 }
