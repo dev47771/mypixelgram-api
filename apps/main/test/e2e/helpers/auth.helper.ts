9cfb909435a40bb6268f693da479e8268a1435f7
Ilya Anisimkov <oblak.00@bk.ru>
2025-10-11 14:36:34 +0500

    fix tests

--- apps/main/test/e2e/helpers/auth.helper.ts
+++ apps/main/test/e2e/helpers/auth.helper.ts
@@ -1,6 +1,6 @@
-import { CreateUserInputDto } from '../../../src/modules/user-accounts/api/input-dto/register-user.input-dto';
+import { RegistrationUserDto } from '../../../src/modules/user-accounts/api/input-dto/register-user.input-dto';
 
-export const correctUser: CreateUserInputDto = {
+export const correctUser: RegistrationUserDto = {
   login: 'Klava009',
   email: 'al.humbatli@gmail.com',
   password: '1234Abc',
