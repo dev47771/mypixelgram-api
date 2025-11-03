22747809aa9ab90e177fa95b1834086bd67dead9
Alik <al.humbatli@gmail.com>
2025-11-03 20:50:16 +0400

    [YAY-30][сonnect to mongo and files collection init] создана новая коллекция в монго под названием FilesSchema. Пока что подключение к бд костыльное, поскольку не настроен конфиг, работает локально в контейнере докера

--- apps/files-api/src/files-api.module.ts
+++ apps/files-api/src/files-api.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { FilesApiController } from './files-api.controller';
 import { FilesApiService } from './files-api.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FileSchema } from './infrastructure/file.schema';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL!),
+    MongooseModule.forFeature([{ name: File.name, schema: FileSchema }]),
+  ],
   controllers: [FilesApiController],
   providers: [FilesApiService],
 })
