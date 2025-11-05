b13b1fe1d39cf8872e68953281c34fde62a5c829
Ilya Anisimkov <oblak.00@bk.ru>
2025-11-05 17:50:31 +0500

    create config in files-api

--- apps/files-api/src/files-api.module.ts
+++ apps/files-api/src/files-api.module.ts
@@ -1,9 +1,17 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { FilesApiController } from './files-api.controller';
 import { FilesApiService } from './files-api.service';
+import { envFilePaths } from './env-file-paths';
+import { validate } from './core/env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: envFilePaths,
+      validate,
+    }),
+  ],
   controllers: [FilesApiController],
   providers: [FilesApiService],
 })
--- apps/files-api/src/main.ts
+++ apps/files-api/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { FilesApiModule } from './files-api.module';
+import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(FilesApiModule);
-  console.log('files-api application started on port 3001');
+  console.log(`files-api application started on port ${process.env.PORT}`);
   await app.listen(process.env.port ?? 3001);
 }
 bootstrap();
