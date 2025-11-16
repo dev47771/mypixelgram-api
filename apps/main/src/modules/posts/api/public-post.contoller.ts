e2c670f1d76d5d735d612e691854e11ef082972f
Alik <al.humbatli@gmail.com>
2025-11-16 10:27:03 +0400

    [YAY-56] some changes in last-posts handle

--- apps/main/src/modules/posts/api/public-post.contoller.ts
+++ apps/main/src/modules/posts/api/public-post.contoller.ts
@@ -8,10 +8,10 @@ export class PublicPostController {
   @LastPostsSwagger()
   async getLastPosts() {
     const files = [
-      { url: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb' },
-      { url: 'https://images.unsplash.com/photo-1465101046530-73398c7f28ca' },
-      { url: 'https://images.unsplash.com/photo-1519125323398-675f0ddb6308' },
-      { url: 'https://images.unsplash.com/photo-1444065381814-865dc9da92c0' },
+      { url: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb', fileId: 'file-101' },
+      { url: 'https://images.unsplash.com/photo-1465101046530-73398c7f28ca', fileId: 'file-102' },
+      { url: 'https://images.unsplash.com/photo-1519125323398-675f0ddb6308', fileId: 'file-103' },
+      { url: 'https://images.unsplash.com/photo-1444065381814-865dc9da92c0', fileId: 'file-104' },
     ];
 
     const posts = [
@@ -19,7 +19,7 @@ export class PublicPostController {
         postId: '1',
         description: 'First public post: short.',
         location: 'Baku',
-        file: files[0],
+        file: [files[0]],
         createdAt: new Date().toISOString(),
         user: {
           userId: 'user-101',
@@ -36,7 +36,7 @@ export class PublicPostController {
           'and anecdotes about meeting new people. Overall, this post contains a full-fledged travel diary with' +
           ' personal thoughts, experiences, and stories, making it five times longer than a generic caption.',
         location: 'Istanbul',
-        file: files[1],
+        file: [files[1]],
         createdAt: new Date().toISOString(),
         user: {
           userId: 'user-102',
@@ -51,7 +51,7 @@ export class PublicPostController {
           ' local food, and the majestic mountains around the city — all this creates a unique flavor. Spent the evening at a' +
           ' cozy cafe overlooking the square. Lots of impressions!',
         location: 'Tbilisi',
-        file: files[2],
+        file: [files[2]],
         createdAt: new Date().toISOString(),
         user: {
           userId: 'user-103',
@@ -67,7 +67,7 @@ export class PublicPostController {
           'Met brilliant people, enjoyed wonderful weather, explored city parks, and finished the day at the ' +
           'British Museum. Loved every minute — so much history here!',
         location: 'London',
-        file: files[3],
+        file: [files[3]],
         createdAt: new Date().toISOString(),
         user: {
           userId: 'user-104',
