7e4defbda2e106fed7fb62c6956d210462b01416
Alik <al.humbatli@gmail.com>
2025-12-02 13:08:49 +0400

    add new cors

--- apps/main/src/setup/app.setup.ts
+++ apps/main/src/setup/app.setup.ts
@@ -10,7 +10,7 @@ export function appSetup(app: INestApplication) {
   pipesSetup(app);
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://mypixelgram.ru'],
+    origin: ['http://localhost:3000', 'https://mypixelgram.ru', 'http://mypixelgram.ru:3000', 'https://mypixelgram.ru:3000'],
     credentials: true,
   });
 }
