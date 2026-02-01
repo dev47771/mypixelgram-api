b6a3ea89ede302983e0f9120aeb5ebcd2d7373f6
Alik <al.humbatli@gmail.com>
2026-02-01 16:27:20 +0400

    added OnModuleInit in rabbitMQ publisher for payments

--- apps/payment/src/payment/jobs/outbox.scheduler.ts
+++ apps/payment/src/payment/jobs/outbox.scheduler.ts
@@ -1,12 +1,13 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { InjectConnection, InjectModel } from '@nestjs/sequelize';
-import { SubscriptionModel } from '../../models/subscription.model';
 import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 import { Sequelize } from 'sequelize-typescript';
+import { SubscriptionModel } from '../../models/subscription.model';
 
 @Injectable()
-export class OutboxScheduler {
+export class OutboxScheduler implements OnModuleInit {
   constructor(
     @InjectModel(SubscriptionModel)
     private subscriptionModel: typeof SubscriptionModel,
@@ -18,28 +19,38 @@ export class OutboxScheduler {
     private rabbitClient: ClientProxy,
   ) {}
 
+  async onModuleInit() {
+    await this.rabbitClient.connect();
+    console.log('[SCHEDULER] RMQ connected, scheduler ready');
+  }
+
   @Cron(CronExpression.EVERY_10_SECONDS)
   async processOutbox() {
-    console.log(' [SCHEDULER] Проверяем outbox...');
+    console.log('[SCHEDULER] Проверяем outbox...');
 
     const subscriptions = await this.subscriptionModel.findAll({
       where: this.sequelize.literal('outboxEvents IS NOT NULL AND JSON_LENGTH(outboxEvents) > 0'),
     });
 
-    console.log(' [SCHEDULER] Найдено:', subscriptions.length);
+    console.log('[SCHEDULER] Найдено:', subscriptions.length);
 
     for (const subscription of subscriptions) {
       const events = subscription.outboxEvents || [];
 
-      for (const event of events) {
-        console.log(' Отправляем:', event.type);
+      try {
+        for (const event of events) {
+          console.log('Отправляем:', event.type);
 
-        this.rabbitClient.emit(event.type, event.payload);
-        console.log('RabbitMQ отправлено!');
+          await firstValueFrom(this.rabbitClient.emit(event.type, event.payload));
+
+          console.log('RabbitMQ отправлено (без ошибки)');
+        }
+
+        await subscription.update({ outboxEvents: [] });
+        console.log('Очистили подписку:', subscription.id);
+      } catch (err) {
+        console.error('[SCHEDULER] Ошибка отправки в RMQ, НЕ очищаем outbox:', err);
       }
-
-      await subscription.update({ outboxEvents: [] });
-      console.log('Очистили подписку:', subscription.id);
     }
   }
 }
