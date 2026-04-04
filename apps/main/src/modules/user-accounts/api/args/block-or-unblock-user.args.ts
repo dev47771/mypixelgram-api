83230323b506c7536e5eb2d2359b5d8e88afeda9
just2BeeAndrew <andrew.dudal.1997@gmail.com>
2026-04-04 22:11:07 +0300

    feat: add block reasons and migration with reasons

--- apps/main/src/modules/user-accounts/api/args/block-or-unblock-user.args.ts
+++ apps/main/src/modules/user-accounts/api/args/block-or-unblock-user.args.ts
@@ -1,4 +1,13 @@
-import { InputType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
-export class BlockUserCommand {}
+export class BlockOrUnblockUserArgs {
+  @Field({ description: 'ID пользователя которого надо заблокировать или разблокировать' })
+  id: string;
+
+  @Field({ nullable: true, description: 'ID жалобы' })
+  reasonId?: number;
+
+  @Field({ nullable: true, description: 'Детали жалобы если указана причина "Another reason"' })
+  reasonDetail?: string;
+}
